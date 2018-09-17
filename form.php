eccfd658fd9415b392c687fc4eb6714ebd04145e
sherliyualinda <42725403+sherliyualinda@users.noreply.github.com>
2018-09-17 09:29:10 +0700

    Buat file form.php

---
+++ form.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>6701174057</title>
+</head>
+<body>
+	<form action="jurnal.php" method="POST" enctype="multipart/form-data">
+		<input type="file" name="file"><br><br>
+		<input type="submit" name="submit" value="kirim">
+	</form>
+</body>
+</html>
