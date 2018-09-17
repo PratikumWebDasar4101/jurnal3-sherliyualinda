72d7ef998e5c821b5a53f5bbb9ac69355473b5c1
sherliyualinda <42725403+sherliyualinda@users.noreply.github.com>
2018-09-17 09:40:36 +0700

    Update form.php

--- form.php
+++ form.php
@@ -5,8 +5,20 @@
 </head>
 <body>
 	<form action="jurnal.php" method="POST" enctype="multipart/form-data">
-		<input type="file" name="file"><br><br>
-		<input type="submit" name="submit" value="kirim">
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<center><table >
+			<tr>
+				<td><input type="file" name="file"></td>
+				<td><input type="submit" name="submit" value="kirim"></td>
+			</tr>
+		
+		
+		</table>
 	</form>
 </body>
 </html>
